ea33438ae529a5d42dddb141eb857a6a3ee213db
Rodion <rodion.groshev@gmail.com>
2025-04-15 18:43:25 +0200

    hw_03 done

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -6,14 +6,20 @@ export const createContactSchema = Joi.object({
   phone: Joi.string()
     .pattern(/^[0-9\-]+$/)
     .required(),
+  favorite: Joi.boolean(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string().email(),
   phone: Joi.string().pattern(/^[0-9\-]+$/),
+  favorite: Joi.boolean(),
 })
   .or("name", "email", "phone")
   .messages({
     "object.missing": "Body must have at least one field",
   });
+
+export const updateStatusContactSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
